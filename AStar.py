b693da372eb2fc64bd8bdf966f1113246f5ec6f5
cjwalle97 <cjwalle97@gmail.com>
2017-03-17 11:42:58 -0500

    Update
    Update

--- AStar.py
+++ AStar.py
@@ -1,9 +1,8 @@
-from drawablenode import DrawableNode
+#from drawablenode import DrawableNode
 from graph import Node
-from graph import Graph
+#from graph import Graph
 import math
 import pygame
-
 
 
 A = Node(0, 0)
@@ -26,3 +25,4 @@ NodeList.insert(1, B)
 NodeList.insert(2, B)
 NodeList.insert(3, C)
 NodeList.insert(4, D)
+
