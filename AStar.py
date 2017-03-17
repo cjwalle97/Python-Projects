20a7be27ea2ada0e53c90da02d025541d4ad42d9
cjwalle97 <cjwalle97@gmail.com>
2017-03-17 17:20:33 -0500

    Update
    Update

--- AStar.py
+++ AStar.py
@@ -13,6 +13,22 @@ d = Node(3, 0, 'd')
 
 e = Node(0, 1, 'e')
 
+f = Node(0, 2, 'f')
+
+g = Node(0, 3, 'g')
+
+h = Node(-1, 0, 'h')
+
+i = Node(-2, 0, 'i')
+
+j = Node(-3, 0, 'j')
+
+k = Node(0, -1, 'k')
+
+l = Node(0, -2, 'l')
+
+m = Node(0, -3, 'm')
+
 a.parent = None
 
 b.parent = a
@@ -22,6 +38,22 @@ c.parent = b
 d.parent = c
 
 e.parent = a
+
+f.parent = e
+
+g.parent = f
+
+h.parent = a
+
+i.parent = h
+
+j.parent = i
+
+k.parent = a
+
+l.parent = k
+
+m.parent = l
 
 def retrace(start, destination):
     path = []
@@ -33,3 +65,8 @@ def retrace(start, destination):
             path.append(destination)
     return path
 
+def printpath(path):
+    for i in path:
+        print 'Node: ', i.name
+
+printpath(retrace(m,a))
