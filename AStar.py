5943715ec2d92d4ed2112c029cf51eb184638f94
cjwalle97 <cjwalle97@gmail.com>
2017-03-23 15:17:36 -0500

    Update
    Continueing to work in NodePractice in order to implement a
    get_neighbors function that I can use for AStar

--- AStar.py
+++ AStar.py
@@ -15,7 +15,7 @@ def retrace(start, destination):
             path.append(destination)
     return path
 
-def astar(start, destination):
+'''def astar(start, destination):
     Children = []
     previous = None
     target = None
@@ -23,17 +23,14 @@ def astar(start, destination):
     while current != destination:
         neighbors = get_neighbors(current, Grid)
         for i in neighbors:
-            if 
-                '''Should make the value of G = 10'''
-            if 
-                '''Should make the value of G = 14'''
+         '''
 
             '''finds the value of H'''
 
             '''Adds G and H to make F then finds the lowest F cost'''
                 
-                '''Moves Current to the node with the lowest F cost'''
-                previous = current
+                '''Moves current to the node with the lowest F cost'''
+               ''' previous = current
                 current = target
                 current.parent = previous
-        
+        '''
