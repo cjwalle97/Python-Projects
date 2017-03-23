a22cec975337ad32127184cc1d9b64ba0e2f5421
cjwalle97 <cjwalle97@gmail.com>
2017-03-23 17:44:00 -0500

    Update
    Replaced all the code in AStar with the code in NodePractice before
    deleting Node Practice.

--- AStar.py
+++ AStar.py
@@ -1,6 +1,27 @@
 from graph import Graph
 
-Grid = Graph(5)
+grid = Graph(3)
+
+class Node(object):
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.parent = None
+        f = None
+        g = None
+        h = None
+
+
+a = Node('a', 0, 0)
+
+b = Node('b', 1, 0)
+
+c = Node('c', 1, 1)
+
+d = Node('d', 0, 1)
+
+e = Node('e', 2, 0)
 
 def retrace(start, destination):
     path = []
@@ -12,26 +33,45 @@ def retrace(start, destination):
             path.append(destination)
     return path
 
+def printpath(path):
+    for i in path:
+        print 'Node: ', i.name
+
+def get_neighbors(current, destination):
+    right = Node('right', current.x + 1, current.y)
+    top_right = Node('top_right', current.x + 1, current.y + 1)
+    top = Node('top', current.x, current.y + 1)
+    top_left = Node('top_left', current.x - 1, current.y + 1)
+    left = Node('left', current.x - 1, current.y)
+    bottom_left = Node('bottom_left', current.x - 1, current.y - 1)
+    bottom = Node('bottom', current.x, current.y - 1)
+    bottom_right = Node('bottom_right', current.x + 1, current.y - 1)
+    neighbors = [right, top_right, top, top_left, left, bottom_left, bottom, bottom_right]
+    lowest = neighbors[0]
+    for i in neighbors:
+        currenttemp = (i.x - current.x) + (i.y - current.y)
+        if currenttemp == 1 or -1:
+            i.g = 10
+        else:
+            i.g = 14
+        desttemp = (destination.x - i.x) + (destination.y - current.y)
+        i.h = desttemp
+        i.f = i.g + i.h
+        if i.f < lowest.f:
+            lowest = i
+    return lowest
+
 def astar(start, destination):
-    Children = []
-    previous = None
-    target = None
     current = start
+    target = None
+    previous = None
     while current != destination:
-        neighbors = get_neighbors(current, Grid)
-        for i in neighbors:
-            
-            if 
-                '''Should make the value of G = 10'''
-            if 
-                '''Should make the value of G = 14'''
+        target = get_neighbors(current, destination)
+        previous = current
+        current = target
+        current.parent = previous
+    return destination
 
-            '''finds the value of H'''
 
-            '''Adds G and H to make F then finds the lowest F cost'''
-                
-                '''Moves current to the node with the lowest F cost'''
-                previous = current
-                current = target
-                current.parent = previous
-        
+astar(a, e)
+
