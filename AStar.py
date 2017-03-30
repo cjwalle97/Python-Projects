d776322a5cf375dd4d68f5f0d21c9d5ac046fb8f
cjwalle97 <cjwalle97@gmail.com>
2017-03-30 16:34:16 -0500

    Update
    Update

--- AStar.py
+++ AStar.py
@@ -5,10 +5,6 @@ class Node(object):
         self.x = x
         self.y = y
         self.parent = None
-        self.g = None
-        self.h = None
-        self.f = None
-
     def __str__(self):
         '''overload string for print'''
         return "Name: " + self.name + "\nPosition: " + str(self.x) + ", " + str(self.y)
@@ -56,113 +52,59 @@ def get_neighbors(current, nodes):
             neighbors.append(i)
     return neighbors
 
-def find_g(node, neighbors):
-    gcosts = []
-    for i in neighbors:
-        xdifference = i.x - node.x
-        ydifference = i.y - node.y
-        totaldifference = xdifference + ydifference
-        if totaldifference == 1 or totaldifference == -1:
-            i.g = 10
-            gcosts.append(i.g)
-        else:
-            i.g = 14
-            gcosts.append(i.g)
-    return gcosts
+def find_g(node, neighbor):
+    xdifference = neighbor.x - node.x
+    ydifference = neighbor.y - node.y
+    totaldifference = xdifference + ydifference
+    if totaldifference == 1 or totaldifference == -1:
+        gcost = 10
+    else:
+        gcost = 14
+    return gcost
 
-def find_h(neighbors, destination):
-    hcosts = []
-    for i in neighbors:
-        xleft = destination.x - i.x
-        if xleft < 1:
-            xleft = xleft * -1
-        yleft = destination.y - i.y
-        if yleft < 1:
-            yleft = yleft * -1
-        totalleft = xleft + yleft
-        i.h = totalleft * 10
-        hcosts.append(i.h)
-    return hcosts
+def find_h(node, destination):
+    xleft = destination.x - node.x
+    if xleft < 1:
+        xleft = xleft * -1
+    yleft = destination.y - node.y
+    if yleft < 1:
+        yleft = yleft * -1
+    totalleft = xleft + yleft
+    hcost = totalleft * 10
+    return hcost
 
-def find_f(neighbors):
-    fcosts = []
-    for i in neighbors:
-        i.f = i.g + i.h
-        fcosts.append(i.f)
-    return fcosts
-
-def find_lowest(neighbors):
-    lowest = neighbors[0]
-    for i in neighbors:
-        if i.f < lowest.f:
-            lowest = i
-    return lowest
-
-def astar(nodes, start, destination):
+def astar(graph, start, goal):
     path = []
     current = start
-    while current != destination:
-        path.append(current)
-        neighbors = get_neighbors(current, nodes)
-        find_g(current, neighbors)
-        find_h(neighbors, destination)
-        find_f(neighbors)
-        next_node = find_lowest(neighbors)
-        previous = current
-        current = next_node
-        current.parent = previous
-    if current == destination:
-        path.append(destination)
-        return path
 
 
 def main():
     '''main'''
-    a = Node('a', 0, 0)
-    b = Node('b', 1, 0)
-    c = Node('c', 1, 1)
-    d = Node('d', 0, 1)
-    e = Node('e', 2, 0)
-    nodes = [a, b, c, d, e]
-    neighbors = get_neighbors(a, nodes)
-    test0 = [b, c, d]  # tests to see if equality
-    
-    testing = True
-    if testing:
-        # test a
-        tests = [test0]
-        i = 0
-        for test in tests:
-            if neighbors == test:
-                success = True
-                print 'pass', "test ", i
-            else:
-                success = False
-                print 'fail', "test ", i
-            i = i + 1
-
-    a.neighbors = neighbors
-    for i in a.neighbors:
-        print i
-
-    gtest = find_g(a,neighbors) #tests to see if the findg function works
-    for i in gtest:
-        print i
-
-    htest = find_h(neighbors, e) #tests to see if the findh function works
-    for i in htest:
-        print i 
-
-    ftest = find_f(neighbors) #tests to see if the findf function works
-    for i in ftest:
-        print i
-    
-    lowtest = find_lowest(neighbors) #tests to see if the findlowestf function works
-    print lowtest
-
-    astartest = astar(nodes, a, e) #tests to see if the astar function works
-    for i in astartest:
-        print i
+    graph = []
+    index = 0
+    for y in range(10):
+        for x in range(10):
+            name = str(index)
+            node = Node(name, x, y)
+            graph.append(node)
+            index += 1
+    openlist = []
+    closedlist = []
+    start = graph[0]
+    goal = graph[50]
+    openlist.append(start)
+    while openlist:
+        current = openlist[0]
+        print current
+        openlist.remove(current)
+        closedlist.append(current)
+        neighbors = get_neighbors(current, graph)
+        if goal in openlist:
+            print 'win'
+            break
+        for neighbor in neighbors:
+            if neighbor not in openlist:
+                openlist.append(neighbor)
 
 if __name__ == '__main__':
 
