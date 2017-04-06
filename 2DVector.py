f7f4bb66dc9c1e53cd82514f60139f4cbb428ec0
cjwalle97 <cjwalle97@gmail.com>
2017-04-06 17:56:52 -0500

    Update
    Adds the add, subtract, and multiply functions to 2dvector

--- 2DVector.py
+++ 2DVector.py
@@ -1,5 +1,19 @@
-class Vector(object):
+class vector(object):
     def __init__(self, xaxis, yaxis):
-        self.x = xaxis
-        self.y = yaxis
+        self.xaxis = xaxis
+        self.yaxis = yaxis
+
+def add(vector1, vector2):
+    result = vector((vector1.xaxis + vector2.xaxis), (vector1.yaxis + vector2.xaxis))
+    return result
+
+def subrtract(vector1, vector2):
+    result = vector((vector1.xaxis - vector2.xaxis), (vector1.yaxis - vector2.yaxis))
+    return result
+
+def multiply(vector1, scalar):
+    result = vector((vector1.xaxis * scalar), (vector1.yaxis * scalar))
+    return result
+
+def magnitude(vector1):
     
