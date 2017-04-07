3fc786b08794c32cb9256c6463e0ae73625a3532
cjwalle97 <cjwalle97@gmail.com>
2017-04-07 15:16:11 -0500

    Update
    Update

--- 2DVector.py
+++ 2DVector.py
@@ -4,32 +4,63 @@ class vector(object):
     def __init__(self, xaxis, yaxis):
         self.xaxis = xaxis
         self.yaxis = yaxis
+    def __str__(self):
+        '''overload string for print'''
+        return str(self.xaxis) + ', ' + str(self.yaxis)
 
-def add(vector1, vector2):
-    result = vector((vector1.xaxis + vector2.xaxis), (vector1.yaxis + vector2.xaxis))
-    return result
+    def __add__(self, other):
+        result = vector((self.xaxis + other.xaxis), (self.yaxis + other.xaxis))
+        return result
 
-def subrtract(vector1, vector2):
-    result = vector((vector1.xaxis - vector2.xaxis), (vector1.yaxis - vector2.yaxis))
-    return result
+    def __sub__(self, other):
+        result = vector((self.xaxis - other.xaxis), (self.yaxis - other.yaxis))
+        return result
 
-def multiply(vector1, scalar):
-    result = vector((vector1.xaxis * scalar), (vector1.yaxis * scalar))
-    return result
 
-def divide(vector1, vector2):
-    result = ((vector1.xaxis / vector2.xaxis), (vector1.yaxis / vector2.yaxis))
-    return result
+    def __mul__(self, scalar):
+        result = vector((self.xaxis * scalar), (self.yaxis * scalar))
+        return result
 
-def magnitude(vector):
-    total = vector.xaxis * vector.yaxis
-    result = math.sqrt(total)
-    return result
 
-def normalize(vector):
-    result = ((vector.xaxis / vector.magnitude), (vector.yaxis / vector.magnitude))
-    return result
+    def __div__(self, other):
+        result = vector((self.xaxis / other.xaxis), (self.yaxis / other.yaxis))
+        return result
 
-def dot_product(vector1, vector2):
-    result = (vector1.xaxis * vector2.xaxis) + (vector1.yaxis * vector2.yaxis)
-    return result
+    def magnitude(self):
+        total = (self.xaxis * self.yaxis)
+        result = math.sqrt(total)
+        return result
+
+    def normalize(self):
+        result = vector((self.xaxis / self.magnitude), (self.yaxis / self.magnitude))
+        return result
+
+    def dot_product(self, other):
+        result = (self.xaxis * other.xaxis) + (self.yaxis * other.yaxis)
+        return result
+
+def main():
+    a = vector(5,5)
+    b = vector(3,3)
+
+    c = a + b
+
+    d = a - b
+
+    e = a * 5
+
+    f = a / b
+
+    g = a.magnitude
+
+    h = a.normalize
+
+    i = a.dot_product(b)
+
+    tests = [c, d, e, f, g, h, i]
+    for j in tests:
+        print j
+
+if __name__ == '__main__':
+    
+    main()
