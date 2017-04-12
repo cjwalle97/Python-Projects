a6b1bc86685321abc9eb7f913b253b1f15fe5b22
cjwalle97 <cjwalle97@gmail.com>
2017-04-12 14:45:30 -0500

    Update
    Update

--- SteeringBehaviour.py
+++ SteeringBehaviour.py
@@ -1,12 +1,26 @@
+import pygame
 from vector import vector
+from vector import magnitude
+from vector import normalize
 
 class agent(object):
     def __init__(self, position):  
         self.position = position
-        self.header = None
+        self.heading = None
         self.mass = 1
         self.velocity = [0, 1]
         self.force = None
 
+class clock():
+    def __init__(self):
+        self.c = game.time.clock()
+        self.deltatime = self.c.pygame.time.tick(30) / 1000.0
+
 def seek(agent, goal):
-    if agent.position
+    maxvelocity = 1
+    v = normalize((goal - agent.position)) * maxvelocity
+    force = v - agent.velocity
+    agent.velocity += force * deltatime
+
+
+'''def flee(agent, goal):'''
