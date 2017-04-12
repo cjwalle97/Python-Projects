0fe491b4427345337bb9b7ca95f1fcca70d44d71
cjwalle97 <cjwalle97@gmail.com>
2017-04-12 12:27:31 -0500

    Update
    Update

--- SteeringBehaviour.py
+++ SteeringBehaviour.py
@@ -3,9 +3,10 @@ from vector import vector
 class agent(object):
     def __init__(self, position):  
         self.position = position
-        self.direction = None
+        self.header = None
         self.mass = 1
         self.velocity = [0, 1]
         self.force = None
 
-def seek(location, agent):
+def seek(agent, goal):
+    if agent.position
